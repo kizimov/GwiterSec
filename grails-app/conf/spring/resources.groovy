d46ca09acd75cff2bdb48a33268a8d0461def9d5
okizimo <kzimow1979@gmail.com>
2021-11-30 14:58:34 +0200

    register CustomUserDetailService

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,5 +1,8 @@
 import gwittersec.PersonPasswordEncoderListener
+import org.springframework.security.core.userdetails.UserDetailsService
+import qwittersec.CustomUserDetailsService
 // Place your Spring DSL code here
 beans = {
     personPasswordEncoderListener(PersonPasswordEncoderListener)
+    UserDetailsService(CustomUserDetailsService)
 }
